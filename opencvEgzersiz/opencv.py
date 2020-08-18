a9598bda00d650f061509d64d440e62f17aa6a97
Furkan Karataş <furkan.karatas1414@gmail.com>
2020-08-18 20:48:25 +0300

    image convert to gray

--- opencvEgzersiz/opencv.py
+++ opencvEgzersiz/opencv.py
@@ -1,8 +1,8 @@
 import cv2
 
 img = cv2.imread(r"images\1.jpg")
-
+gri = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 cv2.imshow("Resim",img)
-
+cv2.imshow("Resi2m",gri)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
